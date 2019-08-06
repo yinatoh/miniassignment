9a7aaa1d9e75b738eb63e1d5b40069c2dead15e3
yinatoh <yiina@live.com>
2019-08-06 11:04:56 +0800

    Create Models and Migrations

--- todoproject/task/models.py
+++ todoproject/task/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_id = models.AutoField(primary_key=True)
+    task_desc = models.TextField()
+    task_com_status = models.BooleanField(default=False)
